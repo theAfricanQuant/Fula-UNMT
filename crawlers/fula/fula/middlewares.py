c0622213c4a1b19ba378215b980a0ae36792409a
Sourcery AI <>
2023-06-06 10:28:06 +0000

    'Refactored by Sourcery'

--- crawlers/fula/fula/middlewares.py
+++ crawlers/fula/fula/middlewares.py
@@ -25,8 +25,7 @@ class FulaSpiderMiddleware:
         # it has processed the response.
 
         # Must return an iterable of Request, dict or Item objects.
-        for i in result:
-            yield i
+        yield from result
 
     def process_spider_exception(self, response, exception, spider):
         # Called when a spider or process_spider_input() method
@@ -42,11 +41,10 @@ class FulaSpiderMiddleware:
         # that it doesn’t have a response associated.
 
         # Must return only requests (not items).
-        for r in start_requests:
-            yield r
+        yield from start_requests
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info(f'Spider opened: {spider.name}')
 
 
 class FulaDownloaderMiddleware:
@@ -93,4 +91,4 @@ class FulaDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info(f'Spider opened: {spider.name}')
